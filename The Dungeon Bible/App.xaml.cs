9dd268a9fd49cf1a1310201ea24da41b712fa86e
Bannlee <tajomarcjerzey@gmail.com>
2026-03-09 01:27:43 +0800

    Add project files.

---
+++ The Dungeon Bible/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace The_Dungeon_Bible
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
